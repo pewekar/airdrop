92f0df4e85c491e6adccb43e755d7c032619a4c0
pewekar <pewekar@gmail.com>
2021-12-24 15:48:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,7 @@ import { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL, Transa
    const newPair = new Keypair();
    const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
    const secretKey = newPair._keypair.secretKey
-/*
    const getWalletBalance = async () => {
-    try {
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const myWallet = await Keypair.fromSecretKey(secretKey);
-        const walletBalance = await connection.getBalance(
-            new PublicKey(myWallet.publicKey)
-        );
-        console.log(`Wallet balance: ${walletBalance}`);
-    } catch (err) {
-        console.log(err);
-    }
-    };
-  */  
-    const getWalletBalance = async () => {
     try {
         const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
         const myWallet = await Keypair.fromSecretKey(secretKey);
@@ -29,20 +15,7 @@ import { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL, Transa
     } catch (err) {
         console.log(err);
     }
-};
-/*
-const airDropSol = async () => {
-    try {
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const walletKeyPair = await Keypair.fromSecretKey(secretKey);
-        const fromAirDropSignature = await connection.requestAirdrop(new PublicKey(walletKeyPair.publicKey),2 * LAMPORTS_PER_SOL);
-        await connection.confirmTransaction(fromAirDropSignature);
-    } catch (err) {
-        console.log(err);
-    }
-};
-*/
-
+   };
 const airDropSol = async () => {
     try {
         const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
@@ -56,7 +29,7 @@ const airDropSol = async () => {
     } catch (err) {
         console.log(err);
     }
-};
+   };
 const driverFunction = async () => {
     await getWalletBalance();
     await airDropSol();
